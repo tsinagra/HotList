e9007c77142f4975c7bff719e68ec82e6b5dadf8
Todd Sinagra <tsinagra@gmail.com>
2016-07-31 16:36:22 -0400

    testing packages
    install for chai and should testing packages

---
+++ tests/tests.js
@@ -0,0 +1,21 @@
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
+
+// synchronous code
+describe('Array Sync Code', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+        [1,2,3].indexOf(5).should.equal(-1);
+        [1,2,3].indexOf(0).should.equal(-1);
+    });
+  });
+});
