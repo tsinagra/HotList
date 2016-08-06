01d596b595559aaab23e17ef98a5f7b490d92e20
Todd Sinagra <tsinagra@gmail.com>
2016-08-06 09:30:20 -0400

    clean up router methods
    cleand up router methods to remove unneeded parameters

--- routes/equipments.js
+++ routes/equipments.js
@@ -5,32 +5,13 @@ var equip = require('../controllers/equipments');
 var monk = require('monk');
 var db = monk('localhost:27017/hotlist');
 
-router.get('/equipment', function(req, res) {
-    equip.findAll(req, res);
-});
-
-router.get('/equipment/:id', function(req, res) {
-    equip.findById(req, res);
-});
-
-router.post('/equipment', function(req, res) {
-    equip.add(req, res);
-});
-
-router.put('/equipment/:id', function(req, res) {
-    equip.update(req, res);
-});
-
-router.post('/equipment/:id/image', function(req, res) {
-    equip.addImage(req, res);
-});
-
-router.delete('/equipment/:id/image', function(req, res) {
-    equip.deleteImage(req, res);
-});
-
-router.delete('/equipment/:id', function(req, res) {
-    equip.delete(req, res);
-});
+router.get('/equipment', equip.findAll);
+router.get('/equipment/:id', equip.findById);
+router.post('/equipment', equip.add);
+router.put('/equipment/:id', equip.update);
+router.post('/equipment/:id/image');
+router.post('/equipment/:id/image', equip.addImage);
+router.delete('/equipment/:id', equip.delete);
+router.delete('/equipment/:id/image', equip.deleteImage);
 
 module.exports = router;
